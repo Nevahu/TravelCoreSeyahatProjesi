7fb3bde297ac174ff431313fff3aeb9b562b31ac
Nevahu <nevahuarslan26@gmail.com>
2024-08-13 15:37:57 +0300

    Layout Kısmı partiallere ayrılarak sadeleştirildi.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklam Kısmını Boş Geçemezsiniz...");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Görsel Seçiniz...");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen en az 50 karakterlik açıklam bilgisi griniz...");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Lütfn karakter sayısını azaltın");
+
+
+        }
+    }
+}
+
