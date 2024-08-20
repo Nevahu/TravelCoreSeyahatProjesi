ec4b05921a9b94b8b0ba73bba775c70b3273144f
Nevahu <nevahuarslan26@gmail.com>
2024-08-20 16:41:53 +0300

    Feature partial için manager ve razor view eklentisi

---
+++ TravelCoreProje/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,22 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCoreProje.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager FeatureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+
+            var values = FeatureManager.TGetlist();
+            return View();
+        }
+    }
+}
