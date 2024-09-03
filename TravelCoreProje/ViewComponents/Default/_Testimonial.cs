44dfb6d005692192538e5f53a75b6b8c2af7ad94
Nevahu <nevahuarslan26@gmail.com>
2024-09-03 13:12:05 +0300

    Testimonial partial yapıldı

--- TravelCoreProje/ViewComponents/Default/_Testimonial.cs
+++ TravelCoreProje/ViewComponents/Default/_Testimonial.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TravelCoreProje.ViewComponents.Default
 {
     public class _Testimonial: ViewComponent
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = testimonialManager.TGetlist();
+            return View(values);
         }
     }
 }
