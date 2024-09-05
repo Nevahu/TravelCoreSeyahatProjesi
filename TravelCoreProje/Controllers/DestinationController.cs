3491e3507a4a580d13486b12d7cd8771d7d90bae
Nevahu <nevahuarslan26@gmail.com>
2024-09-05 14:10:22 +0300

    Destination Controller Başlangıç

---
+++ TravelCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
