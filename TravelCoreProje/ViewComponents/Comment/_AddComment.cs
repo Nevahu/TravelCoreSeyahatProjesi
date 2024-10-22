fa52b99de332a85ac0c7d9bd978dcf677e45baa8
Nevahu <nevahuarslan26@gmail.com>
2024-10-22 22:40:52 +0300

    comment list

---
+++ TravelCoreProje/ViewComponents/Comment/_AddComment.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelCoreProje.ViewComponents.Comment
+{
+    public class _AddComment : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
