a25b24344b2558d11447661d0a44bd2bf81619ae
Nevahu <nevahuarslan26@gmail.com>
2024-10-23 04:05:32 +0300

    güncelleme

---
+++ TravelCoreProje/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TravelCoreProje.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentState = true;
+            p.DestinationID = 3;
+            commentManager.TAdd(p);
+            return RedirectToAction("Index", "Destination");
+        }
+    }
+}
